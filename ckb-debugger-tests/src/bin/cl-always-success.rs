63130211a2d2bce1a6dee61f85bb1f0e01ff7d90
mohanson <mohanson@outlook.com>
2023-04-13 14:58:58 +0800

    Add a small optimization for cl-always-success

--- ckb-debugger-tests/src/bin/cl-always-success.rs
+++ ckb-debugger-tests/src/bin/cl-always-success.rs
@@ -6,7 +6,7 @@ use ckb_types::prelude::Pack;
 use molecule::prelude::{Builder, Entity};
 
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut tx = read_tx_template("../ckb-debugger-tests/templates/cl-always-success.json")?;
+    let tx = read_tx_template("../ckb-debugger-tests/templates/cl-always-success.json")?;
     let mut repr_tx = ReprMockTransaction::from(tx.clone());
 
     let child_script = ChildScript::new_builder()
@@ -24,15 +24,8 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     args.extend(smt_root.as_slice());
 
     repr_tx.mock_info.inputs[0].output.lock.args = ckb_jsonrpc_types::JsonBytes::from_vec(args);
-    tx = repr_tx.into();
-    tx.tx = tx
-        .tx
-        .as_advanced_builder()
-        .set_witnesses(vec![witness_args.pack()])
-        .build()
-        .data();
+    repr_tx.tx.witnesses[0] = ckb_jsonrpc_types::JsonBytes::from(witness_args.pack());
 
-    let repr_tx: ReprMockTransaction = tx.into();
     let json = serde_json::to_string_pretty(&repr_tx).unwrap();
     println!("{}", json);
     Ok(())
