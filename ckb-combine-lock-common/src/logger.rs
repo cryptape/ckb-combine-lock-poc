b8e365a622f9fc0de3bcdfca49964fb01739b9a8
xjd <lynndon@gmail.com>
2023-04-21 16:25:26 +0800

    Add colors to log

--- ckb-combine-lock-common/src/logger.rs
+++ ckb-combine-lock-common/src/logger.rs
@@ -8,18 +8,51 @@ use log::{LevelFilter, SetLoggerError};
 struct SimpleLogger;
 
 impl log::Log for SimpleLogger {
-    fn enabled(&self, metadata: &Metadata) -> bool {
-        metadata.level() <= Level::Info
+    fn enabled(&self, _metadata: &Metadata) -> bool {
+        true
     }
 
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            syscalls::debug(format!(
-                "[{} {}] {}",
-                record.level(),
-                record.target(),
-                record.args()
-            ))
+        let metadata = record.metadata();
+        if self.enabled(metadata) {
+            let level = metadata.level();
+            match level {
+                Level::Error => syscalls::debug(format!(
+                    "[\x1b[31m{}\x1b[0m {}:{}] {}",
+                    record.level(),
+                    record.file().unwrap_or("???"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )),
+                Level::Warn => syscalls::debug(format!(
+                    "[\x1b[33m{}\x1b[0m  {}:{}] {}",
+                    record.level(),
+                    record.file().unwrap_or("???"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )),
+                Level::Info => syscalls::debug(format!(
+                    "[\x1b[32m{}\x1b[0m  {}:{}] {}",
+                    record.level(),
+                    record.file().unwrap_or("???"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )),
+                Level::Debug => syscalls::debug(format!(
+                    "[\x1b[34m{}\x1b[0m {}:{}] {}",
+                    record.level(),
+                    record.file().unwrap_or("???"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )),
+                Level::Trace => syscalls::debug(format!(
+                    "[\x1b[36m{}\x1b[0m {}:{}] {}",
+                    record.level(),
+                    record.file().unwrap_or("???"),
+                    record.line().unwrap_or(0),
+                    record.args()
+                )),
+            }
         }
     }
 
@@ -29,5 +62,5 @@ impl log::Log for SimpleLogger {
 static LOGGER: SimpleLogger = SimpleLogger;
 
 pub fn init() -> Result<(), SetLoggerError> {
-    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Info))
+    log::set_logger(&LOGGER).map(|()| log::set_max_level(LevelFilter::Trace))
 }
--- contracts/child-script-always-success/src/entry.rs
+++ contracts/child-script-always-success/src/entry.rs
@@ -2,7 +2,9 @@ use crate::error::Error;
 use ckb_combine_lock_common::chained_exec::continue_running;
 use ckb_std::env::argv;
 use core::result::Result;
+use log::debug;
 use log::info;
+use log::trace;
 
 pub fn main() -> Result<(), Error> {
     inner_main()?;
@@ -13,6 +15,8 @@ pub fn main() -> Result<(), Error> {
 
 pub fn inner_main() -> Result<(), Error> {
     // always success
+    trace!("trace ....");
+    debug!("debug ....");
     info!("always success!");
     Ok(())
 }
