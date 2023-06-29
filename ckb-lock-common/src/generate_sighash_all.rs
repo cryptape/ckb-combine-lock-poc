479282643debecb51835e05e23afde09949fad90
quake <quake.wang@gmail.com>
2023-06-29 14:12:54 +0900

    fix: resolve lock length > chunks size issue

--- ckb-lock-common/src/generate_sighash_all.rs
+++ ckb-lock-common/src/generate_sighash_all.rs
@@ -1,118 +1,79 @@
 use crate::blake2b::new_blake2b;
 use crate::error::Error;
+use alloc::{vec, vec::Vec};
 use blake2b_rs::Blake2b;
 use ckb_std::ckb_constants::{InputField, Source};
 use ckb_std::high_level::load_tx_hash;
-use ckb_std::syscalls::{load_cell, load_input_by_field, load_witness, SysError};
+use ckb_std::syscalls::{load_input_by_field, load_witness, SysError};
 
-pub const MAX_WITNESS_SIZE: usize = 32768;
-pub const ONE_BATCH_SIZE: usize = 32768;
-
-pub fn load_witness_c_style(
-    buf: &mut [u8],
-    offset: usize,
-    index: usize,
-    source: Source,
-) -> Result<usize, SysError> {
-    match load_witness(buf, offset, index, source) {
-        Ok(size) => Ok(size),
-        Err(SysError::LengthNotEnough(size)) => Ok(size),
-        Err(other) => Err(other),
-    }
-}
+const CHUNK_SIZE: usize = 32768;
 
 pub fn generate_sighash_all() -> Result<[u8; 32], Error> {
-    let mut temp = [0u8; MAX_WITNESS_SIZE];
-
-    // Load witness of first input.
-    let mut read_len = load_witness_c_style(&mut temp, 0, 0, Source::GroupInput)?;
-    let witness_len = read_len;
-    if read_len > MAX_WITNESS_SIZE {
-        read_len = MAX_WITNESS_SIZE;
-    }
-
-    // Load signature.
-    if read_len < 20 {
-        return Err(Error::Encoding);
-    }
-    let lock_length = u32::from_le_bytes(temp[16..20].try_into().unwrap()) as usize;
-    if read_len < 20 + lock_length {
-        return Err(Error::Encoding);
-    }
-    // Clear lock field to zero, then digest the first witness
-    // lock_bytes_seg.ptr actually points to the memory in temp buffer.
-    temp[20..20 + lock_length].fill(0);
-
-    // Load tx hash.
-    let tx_hash = load_tx_hash()?;
-
-    // Prepare sign message.
-    let mut blake2b_ctx = new_blake2b();
-    blake2b_ctx.update(&tx_hash);
-    blake2b_ctx.update(&(witness_len as u64).to_le_bytes());
-    blake2b_ctx.update(&temp[..read_len]);
-
-    // Remaining of first witness.
-    if read_len < witness_len {
-        load_and_hash_witness(&mut blake2b_ctx, read_len, 0, Source::GroupInput, false)?;
-    }
-
-    // Digest same group witnesses.
-    let mut i = 1;
-    loop {
-        let sysret = load_and_hash_witness(&mut blake2b_ctx, 0, i, Source::GroupInput, true);
-        match sysret {
-            Err(SysError::IndexOutOfBound) => break,
-            Err(x) => return Err(x.into()),
-            Ok(_) => i += 1,
+    // Digest first witness in the script group.
+    let mut chunks = ChunksLoader::new(load_witness, CHUNK_SIZE, 0, Source::GroupInput).into_iter();
+    let mut ctx = new_blake2b();
+    if let Some((total_len, mut chunk_data)) = chunks.next() {
+        if total_len < 20 {
+            return Err(Error::Encoding);
         }
+        let lock_length = u32::from_le_bytes(chunk_data[16..20].try_into().unwrap()) as usize;
+        if total_len < lock_length + 20 {
+            return Err(Error::Encoding);
+        }
+        let mut zero_remain = lock_length + 20;
+        if zero_remain > chunk_data.len() {
+            chunk_data[20..].fill(0);
+            zero_remain -= CHUNK_SIZE;
+        } else {
+            chunk_data[20..zero_remain].fill(0);
+            zero_remain = 0;
+        }
+
+        let tx_hash = load_tx_hash()?;
+        ctx.update(&tx_hash);
+        ctx.update(&(total_len as u64).to_le_bytes());
+        ctx.update(&chunk_data);
+        for (_total_len, mut chunk_data) in chunks {
+            if zero_remain > 0 {
+                if zero_remain > chunk_data.len() {
+                    chunk_data[..].fill(0);
+                    zero_remain -= CHUNK_SIZE;
+                } else {
+                    chunk_data[..zero_remain].fill(0);
+                    zero_remain = 0;
+                }
+            }
+            ctx.update(&chunk_data);
+        }
+    } else {
+        return Err(Error::Encoding);
     }
 
+    // Digest other witnesses in the script group.
+    load_and_hash_witness(&mut ctx, 1, Source::GroupInput);
     // Digest witnesses that not covered by inputs.
-    let mut i = calculate_inputs_len()?;
-
-    loop {
-        let sysret = load_and_hash_witness(&mut blake2b_ctx, 0, i, Source::Input, true);
-        match sysret {
-            Err(SysError::IndexOutOfBound) => break,
-            Err(x) => return Err(x.into()),
-            Ok(_) => i += 1,
-        }
-    }
+    let i = calculate_inputs_len()?;
+    load_and_hash_witness(&mut ctx, i, Source::Input);
     let mut msg = [0u8; 32];
-    blake2b_ctx.finalize(&mut msg);
+    ctx.finalize(&mut msg);
     Ok(msg)
 }
 
-fn load_and_hash_witness(
-    ctx: &mut Blake2b,
-    start: usize,
-    index: usize,
-    source: Source,
-    hash_length: bool,
-) -> Result<(), SysError> {
-    let mut temp = [0u8; ONE_BATCH_SIZE];
-    let len = load_witness_c_style(&mut temp, start, index, source)?;
-    if hash_length {
-        ctx.update(&(len as u64).to_le_bytes());
-    }
-    let mut offset = if len > ONE_BATCH_SIZE {
-        ONE_BATCH_SIZE
-    } else {
-        len
-    };
-    ctx.update(&temp[..offset]);
-    while offset < len {
-        let current_len = load_witness_c_style(&mut temp, start + offset, index, source)?;
-        let current_read = if current_len > ONE_BATCH_SIZE {
-            ONE_BATCH_SIZE
+fn load_and_hash_witness(ctx: &mut Blake2b, mut start_index: usize, source: Source) {
+    loop {
+        let mut chunks =
+            ChunksLoader::new(load_witness, CHUNK_SIZE, start_index, source).into_iter();
+        if let Some((total_len, chunk_data)) = chunks.next() {
+            ctx.update(&(total_len as u64).to_le_bytes());
+            ctx.update(&chunk_data);
+            for (_total_len, chunk_data) in chunks {
+                ctx.update(&chunk_data);
+            }
         } else {
-            current_len
-        };
-        ctx.update(&temp[..current_read]);
-        offset += current_read;
+            break;
+        }
+        start_index += 1
     }
-    Ok(())
 }
 
 fn calculate_inputs_len() -> Result<usize, Error> {
@@ -129,18 +90,58 @@ fn calculate_inputs_len() -> Result<usize, Error> {
     Ok(i)
 }
 
-#[allow(dead_code)]
-fn calculate_outputs_len() -> Result<usize, Error> {
-    let mut temp = [0u8; 8];
-    let mut i = 0;
-    loop {
-        let sysret = load_cell(&mut temp, 0, i, Source::Output);
-        match sysret {
-            Err(SysError::IndexOutOfBound) => break,
-            Err(SysError::LengthNotEnough(_)) => i += 1,
-            Err(x) => return Err(x.into()),
-            Ok(_) => i += 1,
+pub struct ChunksLoader<F> {
+    load_fn: F,
+    chunk_size: usize,
+    index: usize,
+    source: Source,
+    offset: usize,
+    len: usize,
+    finished: bool,
+}
+
+impl<F> ChunksLoader<F> {
+    pub fn new(load_fn: F, chunk_size: usize, index: usize, source: Source) -> Self {
+        Self {
+            load_fn,
+            chunk_size,
+            index,
+            source,
+            offset: 0,
+            len: 0,
+            finished: false,
         }
     }
-    Ok(i)
+}
+
+impl<F: Fn(&mut [u8], usize, usize, Source) -> Result<usize, SysError>> Iterator
+    for ChunksLoader<F>
+{
+    type Item = (usize, Vec<u8>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.finished {
+            return None;
+        }
+        let mut buf = vec![0u8; self.chunk_size];
+        match (self.load_fn)(&mut buf, self.offset, self.index, self.source) {
+            Ok(loaded_len) => {
+                self.finished = true;
+                buf.truncate(loaded_len);
+                Some((loaded_len, buf))
+            }
+            Err(SysError::LengthNotEnough(actual_len)) => {
+                self.offset += self.chunk_size;
+                if self.len == 0 {
+                    self.len = actual_len;
+                }
+                Some((self.len, buf))
+            }
+            Err(SysError::IndexOutOfBound) => {
+                self.finished = true;
+                None
+            }
+            Err(_err) => unreachable!(),
+        }
+    }
 }
