fc8aa67bf1fc98b5f34eb2e5d6bd1b10a44d49e8
xjd <lynndon@gmail.com>
2023-07-10 14:16:00 +0800

    Update by review

--- ckb-lock-common/src/intersection.rs
+++ ckb-lock-common/src/intersection.rs
@@ -3,35 +3,30 @@
 use core::ops::Range;
 
 pub fn get_intersection(chunk: Range<usize>, target: Range<usize>) -> Option<Range<usize>> {
-    let chunk_offset = chunk.start;
     let chunk_size = chunk.end - chunk.start;
-    let target_offset = target.start;
-    let target_size = target.end - target.start;
 
-    if target_offset >= chunk_offset {
-        if target_offset < (chunk_offset + chunk_size) {
-            let end = target_offset + target_size;
-            if end >= (chunk_offset + chunk_size) {
+    if target.start >= chunk.start {
+        if target.start < (chunk.start + chunk_size) {
+            if target.end >= (chunk.start + chunk_size) {
                 // case 1:
                 // chunk_begin, signature_begin, chunk_end, signature_end
-                return Some(target_offset - chunk_offset..chunk_size);
+                return Some(target.start - chunk.start..chunk_size);
             } else {
                 // case 2:
                 // chunk_begin, signature_begin, signature_end, chunk_end
-                return Some(target_offset - chunk_offset..end - chunk_offset);
+                return Some(target.start - chunk.start..target.end - chunk.start);
             }
         }
     } else {
-        let end = target_offset + target_size;
-        if end > chunk_offset {
-            if end >= (chunk_offset + chunk_size) {
+        if target.end > chunk.start {
+            if target.end >= chunk.end {
                 // case 3:
                 // signature_begin, chunk_begin, chunk_end, signature_end
                 return Some(0..chunk_size);
             } else {
                 // case 4:
                 // signature_begin, chunk_begin, signature_end, chunk_end
-                return Some(0..end - chunk_offset);
+                return Some(0..target.end - chunk.start);
             }
         }
     }
